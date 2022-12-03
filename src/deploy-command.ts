4f2d1f40af1fcde114dd969ee1d27ffb7bad2eed
chetan chandel <chetanchandel3@gmail.com>
2022-12-04 01:06:49 +0530

    command deployment script

---
+++ src/deploy-command.ts
@@ -0,0 +1,49 @@
+import { REST, Routes } from "discord.js";
+import { clientId, guildId, token } from "./config.json";
+import fs from "node:fs";
+import path from "node:path";
+
+const assertDataLength = (data: unknown): data is { length: number } =>
+  !!data && typeof data === "object" && "length" in data;
+
+const commands = [];
+// Grab all the command files from the commands directory you created earlier
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".ts"));
+
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const { command } = require(filePath);
+  commands.push(command.data.toJSON());
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: "10" }).setToken(token);
+
+// and deploy your commands!
+(async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
+
+    // The put method is used to fully refresh all commands in the guild with the current set
+    const data = await rest.put(
+      // Routes.applicationGuildCommands(clientId, guildId),
+      Routes.applicationCommands(clientId),
+      { body: commands }
+    );
+
+    if (assertDataLength(data)) {
+      console.log(
+        `Successfully reloaded ${data?.length} application (/) commands.`
+      );
+    }
+  } catch (error) {
+    // And of course, make sure you catch and log any errors!
+    console.error(error);
+  }
+})();
